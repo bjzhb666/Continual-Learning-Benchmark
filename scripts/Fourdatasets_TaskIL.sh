5f1c35d54f491aafe007c44d1ef085d5b19e4088
nbl97 <1035833260@qq.com>
2023-01-28 09:58:40 +0800

    fix bug in 4data.sh

--- scripts/Fourdatasets_TaskIL.sh
+++ scripts/Fourdatasets_TaskIL.sh
@@ -4,7 +4,7 @@ REPEAT=1
 mkdir -p $OUTDIR
 python -u iBatchLearn5data.py --gpuid $GPUID --repeat $REPEAT \
 --optimizer Adam  --dataset fourdatasets  \
---force_out_dim 50  \
+--force_out_dim 0  \
 --schedule 40 60 80 --batch_size 64 \
 --model_name ResNet18 --model_type resnet \
 --agent_type customization  --agent_name EWC        \
