caf259728f6de300c985bd6937ead889bf5fe0f8
nbl97 <1035833260@qq.com>
2023-01-31 22:09:20 +0800

    fix bugs on wandb name

--- scripts/Fourdatasets_TaskIL.sh
+++ scripts/Fourdatasets_TaskIL.sh
@@ -5,7 +5,7 @@ mkdir -p $OUTDIR
 python -u iBatchLearn5data.py --gpuid $GPUID --repeat $REPEAT \
 --optimizer Adam  --dataset fourdatasets  \
 --force_out_dim 0  \
---schedule 40 60 80 --batch_size 64 \
+--schedule 80 120 160 --batch_size 64 \
 --model_name ResNet18 --model_type resnet \
 --agent_type customization  --agent_name EWC        \
---lr 0.001 --reg_coef 10       | tee ${OUTDIR}/EWC.log
+--lr 0.001 --reg_coef 1       | tee ${OUTDIR}/ EWC_reg_coef=1.log
